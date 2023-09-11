bcf57a67e410ff6b3c22030a9fa7821939c0f301
Absarahmed77 <144701628+Absarahmed77@users.noreply.github.com>
2023-09-11 19:31:49 +0500

    Add files via upload

---
+++ Qno 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+printf(" Name          : Absar Ahmed\n");
+printf(" father's Name : Gulzar Ahmed\n");
+printf(" Cast           : Memon   \n");
+printf(" age           :  20 years\n");
+printf(" grade         : A\n");
+
+}
